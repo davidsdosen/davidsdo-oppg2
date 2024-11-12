80fd2ae53e1121d2059b3899a8124c0a3525fc32
davidsdosen <david.sorhaug.dosen@gmail.com>
2024-11-12 04:24:07 +0100

    minimum_tls_version = "TLS1_2"

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -6,6 +6,7 @@ resource "azurerm_mssql_server" "sqlserver" {
     administrator_login          = var.sql_admin_username
     administrator_login_password = var.sql_admin_password
     public_network_access_enabled = false
+    minimum_tls_version = "TLS1_2"
 }
 
 resource "azurerm_mssql_database" "sqldatabase" {
