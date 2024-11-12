6e4624388273be6c259425f4af5cb955eaa577cf
davidsdosen <david.sorhaug.dosen@gmail.com>
2024-11-12 04:08:53 +0100

    public_network_access_enabled = false in sqlserver

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -5,6 +5,7 @@ resource "azurerm_mssql_server" "sqlserver" {
     version                      = "12.0"
     administrator_login          = var.sql_admin_username
     administrator_login_password = var.sql_admin_password
+    public_network_access_enabled = false
 }
 
 resource "azurerm_mssql_database" "sqldatabase" {
